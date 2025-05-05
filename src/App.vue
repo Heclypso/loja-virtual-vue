258b2634754fefd4cbdd1e4417bc3cc10ec3a817
Heclypso <heclypso@gmail.com>
2025-05-05 15:26:24 -0300

    #1 feat: desenvolvido container

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Navbar></Navbar>
+  <div class="container">oi</div>
 </template>
 
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #E9E9E9;
+}
+</style>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.container {
+  width: 90vw;
+  height: 662px;
+  margin: 56px 80px;
+  background-color: #fff;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="nav">
+        <img src="../assets/supermarket-logo.svg" alt="Logo do supermercado" class="nav__logo">
+        <div class="nav__container">
+            <img src="../assets/shopping-cart-icon.svg" alt="Ícone do carrinho de compras" class="nav__icon">
+            <span class="nav__icon__counter">0</span>
+        </div>
+        
+    </nav>
+</template>
+
+<style scoped>
+.nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 74px;
+    background-color: #fff;
+    border-bottom: 1px solid #D5D5D5;
+    padding: 8px 80px;
+}
+
+.navbar__container {
+    position: relative;
+}
+
+.nav__logo {
+    height: 57px;
+}
+
+.nav__icon {
+    width: 38px;
+    height: 38px;
+    position: relative;
+}
+
+.nav__icon__counter {
+    position: absolute;
+    top: 0;
+    right: 0;
+}
+</style>
