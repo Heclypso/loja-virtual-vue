c93e8a9c496841012a8303db7e8708fdd397b3a1
Heclypso <heclypso@gmail.com>
2025-05-06 19:45:08 -0300

    #2 feat: aplicado fonte customizada

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,11 @@
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: "Work Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
 }
+
 
 body {
   background-color: #E9E9E9;
