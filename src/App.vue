afa04257f99ebceec3b79c7a08124f87e300dabb
Heclypso <heclypso@gmail.com>
2025-05-13 21:05:26 -0300

    #7 feat: desenvolvido os botões de filtro

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,13 @@
   <Navbar></Navbar>
   <div class="container">
     <h1 class="container__title">Sessão de Itens</h1>
+    <div class="container__filter">
+        <button class="container__filter__button active">Todos os Produtos</button>
+        <button class="container__filter__button">Mais caros</button>
+        <button class="container__filter__button">Mais baratos</button>
+        <button class="container__filter__button">Mais vendidos</button>
+        <button class="container__filter__button">Melhor avaliado</button>
+    </div>
     <Product></Product>
     <Product></Product>
     <Product></Product>
@@ -49,5 +56,29 @@ body {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.container__filter {
+  height: 40px;
+  background-color: #F9F9F9;
+}
+
+.container__filter__button {
+  height: inherit;
+  width: 20%;
+  border-radius: 0px;
+  border: none;
+  border-right: 1px solid #dfdfdf;
+  font-size: 24px;
+  color: #C5C5C5;
+  font-weight: 300;
+  }
+
+.active {
+  background-color: #fff;
+  color: #202020;
+  border: none;
+  border-bottom: 2px solid #202020;
+  font-weight: 400;
 }
 </style>
